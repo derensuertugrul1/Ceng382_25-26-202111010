7a2f22e0d55672b701c79393025450d72a7db46a
derensuertugrul <derensuertugrul@gmail.com>
2025-03-26 15:15:55 +0300

    week 5 finished.

--- RazorPagesApp/obj/Debug/net9.0/RazorPagesApp.AssemblyInfo.cs
+++ RazorPagesApp/obj/Debug/net9.0/RazorPagesApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RazorPagesApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+44a79b267f989f12247f6b5caafdfd117c225211")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+187a3ff49e98e8e64e08462fe0adbfc6e5debc2d")]
 [assembly: System.Reflection.AssemblyProductAttribute("RazorPagesApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RazorPagesApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
